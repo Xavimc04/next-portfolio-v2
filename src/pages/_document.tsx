a97b6388beb1483f1348df4a9822a5aeac765fdb
Xavimc04 <xaviermorcam@campus.monlau.com>
2023-09-11 02:36:31 +0200

    Favicon & webside header

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -4,6 +4,8 @@ export default function Document() {
     return (
         <Html lang="en">
             <Head>
+                <title>Xavier Morell | 2023 Portfolio</title>
+                <link rel="icon" type="image/x-icon" href="https://talently.tech/blog/wp-content/uploads/2023/01/sueldo-de-un-programador.png" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined" />
                 <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Icons" />
             </Head>
